8b4e07eef983d93d6eee0fa0ff2fe54bb741f67d
KelvynNeris <kelvyn.neris4305@gmail.com>
2025-04-23 13:06:24 -0300

    resolvendo o erro da virgula

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const operadores = document.querySelectorAll(".operador");
 const igual = document.querySelector(".igual");
 
 let expressao = "";
+let expressao_calc = "";
 let entradaAtual = "";
 
 // Quando clica em um número
@@ -24,6 +25,7 @@ operadores.forEach(operador => {
             conta.textContent = "";
             resultado.textContent = "0";
             expressao = "";
+            expressao_calc = "";
             entradaAtual = "";
             return;
         }
@@ -46,12 +48,98 @@ operadores.forEach(operador => {
             return;
         }
 
+        // trata a vírgula ANTES de mexer com a expressão
+        if (op === ",") {
+            if (!entradaAtual.includes(",")) {
+                entradaAtual += ",";
+                const ponto = entradaAtual.replace(",", ".");
+                expressao_calc = ponto
+                resultado.textContent = entradaAtual;
+            }
+            return; // impede que o código abaixo execute
+        }
+
+
         // Se for um operador normal (+, -, *, /, etc.)
         if (entradaAtual) {
             expressao += entradaAtual + op;
+            expressao_calc += entradaAtual + op;
             conta.textContent = expressao;
+            conta.textContent = expressao_calc;
             entradaAtual = "";
             resultado.textContent = "0";
         }
+
+        if (op === "×") {
+            expressao_calc = expressao_calc.slice(0, -1)
+            expressao_calc += "*";
+        }
+
+        else if (op === "÷") {
+            expressao_calc += "/";
+        }
+
+        else if (op === "√") {
+            expressao = expressao.slice(0, -1)
+            let ultimoCaractere = expressao.slice(-1)
+            console.log(expressao)
+            expressao = expressao.slice(0, -1)
+            expressao += `√(${ultimoCaractere})`;
+            conta.textContent = expressao
+
+            expressao_calc = expressao_calc.slice(0, -2)
+            expressao_calc += `${Math.sqrt(ultimoCaractere)}`;
+            console.log(expressao_calc)
+        }
+
+        else if (op === "x²") {
+            expressao = expressao.slice(0, -2)
+            expressao += "²"
+            conta.textContent = expressao
+
+            expressao_calc = expressao_calc.slice(0, -2)
+            expressao_calc += "**2";
+        }
+
+        else if (op === "1/x") {
+            expressao = expressao.slice(0, -3)
+            let ultimoCaractere = expressao.slice(-1)
+            expressao = expressao.slice(0, -1)
+            console.log(expressao, ultimoCaractere)
+            expressao += `1/(${ultimoCaractere})`;
+        }
+
+        else if (op === "%") {
+            expressao = expressao.slice(0, -1)
+            let ultimoCaractere = expressao.slice(-1)
+            expressao = expressao.slice(0, -1)
+            let porcentagem = ultimoCaractere / 100
+            expressao += porcentagem
+            resultado.textContent = porcentagem
+            expressao_calc += "/100";
+        }
+
+        else {
+            expressao += op;
+        }
+
+        conta.textContent = expressao
     });
+});
+
+igual.addEventListener("click", () => {
+    const sinal = igual.textContent;
+
+    if (sinal === "=") {
+        expressao += entradaAtual;
+        expressao_calc += entradaAtual;
+        conta.textContent = expressao + "="
+        console.log(expressao, expressao_calc)
+
+        const res = eval(expressao_calc);
+        resultado.textContent = res;
+
+        expressao = "";
+        expressao_calc = "";
+    }
 });
